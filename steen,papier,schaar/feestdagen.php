f4d273a63db85a0dabbd8a537fa95807da0ea6d3
finn bloemers <finn.bloemers@student.gildeopleidingen.nl>
2026-06-17 11:50:33 +0200

    Add files via upload

---
+++ steen,papier,schaar/feestdagen.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="#"> 
+</head>
+<body>
+
+<?php
+     $db_server = "localhost";
+     $db_user = "root";
+     $db_pass = "";
+     $db_name = "feestdagen";
+     $conn = "";
+
+     $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+     
+     
+    $sql = "SELECT feestdag, datum, maand FROM feestdagen";
+     $result = $conn->query($sql);
+
+if ($result && $result->num_rows > 0) {
+
+    echo "<table border='1' cellpadding='10'>";
+
+    // kolom namen
+    echo "<tr>";
+    echo "<th>feestdag</th>";
+    echo "<th>datum</th>";
+    echo "<th>maand</th>";
+    echo "</tr>";
+
+    // data uit database
+    while($row = $result->fetch_assoc()) {
+
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($row['feestdag']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['datum']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['maand']) . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+
+} else {
+    echo "0 resultaten";
+}
+
+$conn->close();
+        
+        ?>
+
+</body>
+</html>
---
+++ steen,papier,schaar/steen_papier_schaar_v2/steen,papier,schaar.js
@@ -0,0 +1,53 @@
+const humenoutput = document.querySelector("#human");
+const computeroutput = document.querySelector("#computer");
+const resultoutput = document.querySelector("#result");
+
+humenoutput.innerHTML = "jouw keuze komt hier, select je keuze";
+
+const steenbtn = document.querySelector("#steen");
+const papierbtn = document.querySelector("#papier");
+const schaarbtn = document.querySelector("#schaar");
+
+let humenchoice = "";
+let computerchoice = "";
+
+
+function getComputerChoice() {
+    const randomnumber = Math.floor(Math.random() * 3) + 1;
+
+    if (randomnumber === 1) return "steen";
+    if (randomnumber === 2) return "papier";
+    return "schaar";
+}
+
+
+function playGame(event) {
+    humenchoice = event.target.id;
+    humenoutput.innerHTML = humenchoice;
+
+    computerchoice = getComputerChoice();
+    computeroutput.innerHTML = computerchoice;
+
+    resultoutput.innerHTML = getResult(humenchoice, computerchoice);
+}
+
+
+steenbtn.addEventListener("click", playGame);
+papierbtn.addEventListener("click", playGame);
+schaarbtn.addEventListener("click", playGame);
+
+function getResult(humenchoice, computerchoice) {
+    if (humenchoice === computerchoice) {
+        return "Gelijkspel ";
+    }
+
+    if (
+        (humenchoice === "steen" && computerchoice === "schaar") ||
+        (humenchoice === "papier" && computerchoice === "steen") ||
+        (humenchoice === "schaar" && computerchoice === "papier")
+    ) {
+        return "Jij wint ";
+    }
+
+    return "Computer wint ";    
+}
